ae611637b040a2b8a820ba26e51d2f1d997cd179
ALVARO <alvaro.vazquez@redradix.com>
2020-05-08 17:20:10 +0200

    Add round button

--- src/App.js
+++ src/App.js
@@ -6,37 +6,47 @@ function App() {
   const [rows, setRows] = useState(5);
   const [columns, setColumns] = useState(5);
   const [aliveCells, setAliveCells] = useState('');
-  const [isStarted, setIsStarted] = useState(false)
-  console.log(rows);
+  const [isStarted, setIsStarted] = useState(false);
 
   const generateGrid = () => {
-    return ([...Array(rows).keys()].map((row) => (
+    return [...Array(rows).keys()].map((row) => (
       <div className='gridRow' key={row}>
         {[...Array(columns).keys()].map((column) => (
-          <div className='gridColumn' key={row}>
-            {aliveCells.some(cell => cell[0] === row && cell[1] === column) ? 'o' : '.'}
+          <div className='gridColumn' key={`${row}-${column}`}>
+            {aliveCells.some((cell) => cell[0] === row && cell[1] === column)
+              ? 'o'
+              : '.'}
           </div>
         ))}
       </div>
-    )))
-  }
+    ));
+  };
+
+  const isAlive = ([x, y]) => 
+    aliveCells.some((cell) => cell[0] === x && cell[1] === y)
 
   const calculateRound = () => {
-    [...Array(rows).keys()].map(row => {
-      [...Array(columns).keys()].map(column => {
+    [...Array(rows).keys()].map((row) => {
+      [...Array(columns).keys()].map((column) => {
+        console.log(`--> [${row}-${column}]`)
+        const neighbors = getNeighbors([row, column])
+        // console.log(neighbors.map((neighbor) => isAlive(neighbor)))
+        const aliveNeighbors = neighbors.map((neighbor) => isAlive(neighbor))
+          .filter(aliveNeighbor => aliveNeighbor).length
+        console.log(aliveNeighbors)
+      });
+    });
+  };
 
-      })
-    })
-  }
-
-  const getNeighbors = ({x, y}) => {
-
+  const getNeighbors = ([ x, y ]) => {
     return (
-      [x-1, y-1], [x, y-1], [x+1, y-1],
-      [x-1, y],             [x+1, y],
-      [x-1, y+1], [x, y+1], [x+1, y+1]
-    )
-  } 
+      [
+        [x - 1, y - 1], [x, y - 1], [x + 1, y - 1],
+        [x - 1, y], [x + 1, y], 
+        [x - 1, y + 1], [x, y + 1], [x + 1, y + 1]
+      ]
+    );
+  };
 
   return (
     <div className='App'>
@@ -52,13 +62,17 @@ function App() {
       />
       <input
         type='text'
-        onChange={(event) => setAliveCells(event.target.value.split('/').map(coords => coords.split(',').map(coord => +coord)))}
+        onChange={(event) =>
+          setAliveCells(
+            event.target.value
+              .split('/')
+              .map((coords) => coords.split(',').map((coord) => +coord))
+          )
+        }
       />
       <button onClick={() => setIsStarted(true)}>Start</button>
       <button onClick={calculateRound}>Round</button>
-      <div>
-        {isStarted && generateGrid()}
-      </div>
+      <div>{isStarted && generateGrid()}</div>
     </div>
   );
 }
