115b73dbb9515df9b88917db2cef17113c35e254
Álvaro Cuesta <alvaro@redradix.com>
2020-05-08 17:16:34 +0200

    Game of life algorithm

--- src/App.js
+++ src/App.js
@@ -5,11 +5,69 @@ const DEFAULT_WIDTH = 10
 const DEFAULT_HEIGHT = 10
 
 const initGameOfLifeGrid = (width, height) =>
-  Array(width)
+  Array(height)
     .fill()
-    .map(() => Array(height).fill(false))
+    .map(() =>
+      Array(width)
+        .fill()
+        .map(() => Math.random() > 0.7),
+    )
 
-console.log(initGameOfLifeGrid(10, 10))
+const stepGameOfLife = grid => {
+  const newGrid = grid.map(row => [...row])
+
+  const height = newGrid.length
+
+  for (let y = 0; y < height; y++) {
+    const width = newGrid[y].length
+
+    for (let x = 0; x < width; x++) {
+      const isAlive = grid[y][x]
+
+      let aliveNeighbors = 0
+
+      for (let dy = -1; dy <= 1; dy++) {
+        for (let dx = -1; dx <= 1; dx++) {
+          const xx = x + dx
+
+          if (xx < 0 || xx >= width) {
+            continue
+          }
+
+          const yy = y + dy
+
+          if (yy < 0 || yy >= height) {
+            continue
+          }
+
+          const isNeighborAlive = grid[yy][xx]
+
+          if (isNeighborAlive) {
+            aliveNeighbors++
+          }
+        }
+      }
+
+      // Any live cell with two or three live neighbors survives.
+      if (isAlive && (aliveNeighbors === 2 || aliveNeighbors === 3)) {
+        newGrid[y][x] = true
+      }
+      // Any dead cell with three live neighbors becomes a live cell.
+      else if (!isAlive && aliveNeighbors === 3) {
+        newGrid[y][x] = true
+      }
+      // All other live cells die in the next generation.
+      else if (isAlive) {
+        newGrid[y][x] = false
+      }
+      // Similarly, all other dead cells stay dead.
+      else {
+      }
+    }
+  }
+
+  return newGrid
+}
 
 function App() {
   const [width, setWidth] = useState(DEFAULT_WIDTH)
@@ -34,15 +92,14 @@ function App() {
   }
 
   const handleStep = () => {
-    alert('step')
+    const newGrid = stepGameOfLife(grid)
+
+    setGrid(newGrid)
   }
 
   const handleReset = () => {
-    alert('reset')
-
-    const grid = initGameOfLifeGrid(width, height)
-
-    setGrid(grid)
+    const newGrid = initGameOfLifeGrid(width, height)
+    setGrid(newGrid)
   }
 
   return (
