f589f6ce2dc7b0e8794db42558980ee6a16392b5
Andres Martin <monkeyandres@protonmail.com>
2020-05-08 16:15:33 +0200

    Pomodoro 1

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
+
+const pauseGame = () => {}
+
+const resetGame = () => {}
 
 function App() {
+  const [board, setBoard] = useState([])
+
+  const [rows, setRows] = useState(5)
+  const [columns, setColumns] = useState(5)
+
+  const startGame = () => {
+    const _board = []
+
+    for (let i = 0; i < rows; i++) {
+      _board.push(Array(Number(columns)).fill('false'))
+    }
+
+    setBoard(_board)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={startGame}>Start</button>
+      <button onClick={pauseGame}>Pause</button>
+      <button onClick={resetGame}>Reset</button>
+
+      <input
+        placeholder="rows"
+        value={rows}
+        onChange={(e) => setRows(e.target.value)}
+      />
+
+      <input
+        placeholder="columns"
+        value={columns}
+        onChange={(e) => setColumns(e.target.value)}
+      />
+
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateRows: `repeat(${rows}, 1fr)`,
+          gridTemplateColumns: `repeat(${columns}, 1fr)`,
+        }}
+        className="board"
+      >
+        {board.map((row, x) =>
+          row.map((cell, y) => (
+            <div>
+              {x}
+              {y}
+            </div>
+          ))
+        )}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
